55d19fd3f937ca21b3d0af7d53dbedaadc805267
Brian Duggan <bduggan@matatu.org>
2016-10-11 10:27:37 -0400

    typo in Build.pm

--- Build.pm
+++ Build.pm
@@ -9,7 +9,7 @@ class Build is Panda::Builder {
         my %vars = get-vars($dir);
         %vars<sha1> = $*VM.platform-library-name('sha1'.IO);
         mkdir "$dir/resources" unless "$dir/resources".IO.e;
-        mkdir "$dir/resources/lib" unless "$dir/resources/lib".IO.e;
+        mkdir "$dir/resources/libraries" unless "$dir/resources/libraries".IO.e;
         process-makefile($dir, %vars);
         my $goback = $*CWD;
         chdir($dir);
